9defa37513f5b0bb74cc2a85eb5a4536db87f9b0
Mustafa Toprak <damonatabay@gmail.com>
2022-06-26 18:37:05 +0200

    rough design for sidebar done

---
+++ components/sidebar.tsx
@@ -0,0 +1,134 @@
+import NextImage from 'next/image';
+import NextLink from 'next/link';
+
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from '@chakra-ui/layout';
+
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from 'react-icons/md';
+
+const navMenu = [
+  {
+    name: 'Home',
+    icon: MdHome,
+    route: '/',
+  },
+  {
+    name: 'Search',
+    icon: MdSearch,
+    route: '/search',
+  },
+  {
+    name: 'Your Library',
+    icon: MdLibraryMusic,
+    route: '/library',
+  },
+];
+
+const musicMenu = [
+  {
+    name: 'Create Playlist',
+    icon: MdPlaylistAdd,
+    route: '/',
+  },
+  {
+    name: 'Favorites',
+    icon: MdFavorite,
+    route: '/favorites',
+  },
+];
+
+const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`);
+
+const Sidebar = () => {
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="gray"
+    >
+      <Box paddingY="20px" height="100%">
+        <Box width="120px" marginBottom="20px" paddingX="20px">
+          <NextImage src="/logo.png" height={60} width={120} />
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {navMenu.map((el) => {
+              return (
+                <ListItem paddingX="20px" fontSize="16px" key={el.name}>
+                  <LinkBox>
+                    <NextLink href={el.route} passHref>
+                      <LinkOverlay>
+                        <ListIcon
+                          as={el.icon}
+                          color="white"
+                          marginRight="20px"
+                        />
+                        {el.name}
+                      </LinkOverlay>
+                    </NextLink>
+                  </LinkBox>
+                </ListItem>
+              );
+            })}
+          </List>
+        </Box>
+        <Box>
+          <List spacing={2}>
+            {musicMenu.map((el) => {
+              return (
+                <ListItem paddingX="20px" fontSize="16px" key={el.name}>
+                  <LinkBox>
+                    <NextLink href={el.route} passHref>
+                      <LinkOverlay>
+                        <ListIcon
+                          as={el.icon}
+                          color="white"
+                          marginRight="20px"
+                        />
+                        {el.name}
+                      </LinkOverlay>
+                    </NextLink>
+                  </LinkBox>
+                </ListItem>
+              );
+            })}
+          </List>
+        </Box>
+        <Divider color="gray.800" paddingY="20px" />
+        <Box height="65%" overflowY="auto" paddingY="20px">
+          <List spacing={2}>
+            {playlists.map((el) => {
+              return (
+                <ListItem paddingX="20px" key={el}>
+                  <LinkBox>
+                    <NextLink href="/" passHref>
+                      <LinkOverlay>{el}</LinkOverlay>
+                    </NextLink>
+                  </LinkBox>
+                </ListItem>
+              );
+            })}
+          </List>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Sidebar;
---
+++ pages/_app.tsx
@@ -0,0 +1,43 @@
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import Playerlayout from '../components/playerLayout';
+import 'reset-css';
+
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: '#F5f5f5',
+      200: '#EEEEEE',
+      300: '#E0E0E0',
+      400: '#BDBDBD',
+      500: '#9E9E9E',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121',
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ':focus': {
+            outline: 'none',
+            boxShadow: 'none',
+          },
+        },
+      },
+    },
+  },
+});
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Playerlayout>
+        <Component {...pageProps} />
+      </Playerlayout>
+    </ChakraProvider>
+  );
+};
+
+export default MyApp;
